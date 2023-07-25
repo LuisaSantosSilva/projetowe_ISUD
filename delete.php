b92c13c0f87518a671023cb591b2fc685dfa715b
LuisaSantosSilva <luisasantossilvaa@gmail.com>
2023-07-25 08:49:17 -0300

    Projeto com Banco de Dados

--- delete.php
+++ delete.php
@@ -1,15 +1,97 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <!-- Titúlo da página -->
+    <title>Document</title>
+</head>
 
-include("conexao.php");
+<!-- Edição do corpo do código HTML, Coloração do Fundo da página e edição da posição do texto  -->
+<body style="background-color:rgb(0, 71, 171)">
+    <div class="container text-center">
+        <!-- Linha e Coluna criada para página responsiva -->
+        <div class="row">
+            <div class="col-1">
+                &nbsp;
+            </div>
+            <!-- Barra de Navegação: -->
+            <div class="col" style="background-color:rgba(48, 48, 48, 0.5)" >
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid">
+                        <!-- Barra de Navegação com o nome SISTEMA WEB -->
+                        <!-- OBS: navbar-brand para o nome de seu projeto, produto ou companhia -->
+                        <a class="navbar-brand" href="#">SISTEMA WEB</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- OBS: navbar-nav para obter uma leve navegação com suporte a dropdowns -->
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <!-- Link da barra de Navegação-->
+                                <li class="nav-item">
+                                <a class="nav-link" href="formulario.php">Gravar dados</a>
+                                </li>
+                                 <!-- Link da barra de Navegação-->
+                                 <li class="nav-item">
+                                <a class="nav-link" href="select.php">Select</a>
+                                </li>
+                                 <!-- Link da barra de Navegação-->
+                                <li class="nav-item">
+                                <a class="nav-link" href="update.php">Update</a>
+                                </li>
+                                <!-- Link da barra de Navegação-->
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Delete</a>
+                                </li> 
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <!-- Coluna criada para página responsiva -->
+            <div class="col-1">
+                &nbsp;
+            </div>
+        </div>
+        <!-- Linha e coluna criada para página responsiva -->
+        <div class="row">
+            <div class="col-1">
+                &nbsp;
+            </div>
+            <!-- Cor do Fundo da página de Formulário e edição de texto-->
+            <div class="col bg-white p-4">
+                
+                <!-- Titúlo e subtitúlo do formulário: -->
+                <p class="text-start fs-5"><strong>Excluir</strong></p>
+                <p class="text-start fs-6">(Delete)</p>
+                <p>
+               
+                <?php
 
-$sql = "DELETE FROM clientes WHERE id=1";
+                    include("conexao.php");
 
-$stmt = $PDO->prepare($sql);
+                    $sql = "DELETE FROM clientes WHERE id=1";
 
-if($stmt->execute()){
-    echo "Dados excluidos com sucesso!";
-}else{
-    echo "Erro ao excluir os dados";
-}
+                    $stmt = $PDO->prepare($sql);
 
-?>
+                    if($stmt->execute()){
+                        echo "Dados excluidos com sucesso!";
+                    }else{
+                        echo "Erro ao excluir os dados";
+                    }
+
+                ?>
+         
+
+            </div>
+            <!-- Coluna criada para página responsiva -->
+            <div class="col-1">
+                &nbsp;
+            </div>
+        </div>
+    </div> 
+</body>
--- formulario.php
+++ formulario.php
@@ -4,23 +4,94 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <!-- Titúlo da página -->
     <title>Document</title>
 </head>
-<body>
-    <form name="cadastro" method="GET" action="resposta.php">
-        Nome: <input type="text" name="nome">
-        <br><br>
-        Endereço: <input type="text" name="endereco">
-        <br><br>
-        Bairro: <input type="text" name="bairro">
-        <br><br>
-        CEP: <input type="text" name="cep">
-        <br><br>
-        Estado: <input type="text" name="estado">
-        <br><br>
-        Cidade: <input type="text" name="cidade">
-        <br><br>
-        <input type="submit" name="cadastrar">
-    </form>
+
+<!-- Edição do corpo do código HTML, Coloração do Fundo da página e edição da posição do texto  -->
+<body style="background-color:rgb(0, 71, 171)">
+    <div class="container text-center">
+        <!-- Linha e Coluna criada para página responsiva -->
+        <div class="row">
+            <div class="col-1">
+                &nbsp;
+            </div>
+            <!-- Barra de Navegação: -->
+            <div class="col" style="background-color:rgba(48, 48, 48, 0.5)" >
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid">
+                        <!-- Barra de Navegação com o nome SISTEMA WEB -->
+                        <!-- OBS: navbar-brand para o nome de seu projeto, produto ou companhia -->
+                        <a class="navbar-brand" href="#">SISTEMA WEB</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- OBS: navbar-nav para obter uma leve navegação com suporte a dropdowns -->
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <!-- Link da barra de Navegação-->
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Gravar dados</a>
+                                </li>
+                                 <!-- Link da barra de Navegação-->
+                                 <li class="nav-item">
+                                <a class="nav-link" href="select.php">Select</a>
+                                </li>
+                                 <!-- Link da barra de Navegação-->
+                                <li class="nav-item">
+                                <a class="nav-link" href="update.php">Update</a>
+                                </li>
+                                <!-- Link da barra de Navegação-->
+                                <li class="nav-item">
+                                <a class="nav-link" href="delete.php">Delete</a>
+                                </li> 
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <!-- Coluna criada para página responsiva -->
+            <div class="col-1">
+                &nbsp;
+            </div>
+        </div>
+        <!-- Linha e coluna criada para página responsiva -->
+        <div class="row">
+            <div class="col-1">
+                &nbsp;
+            </div>
+            <!-- Cor do Fundo da página de Formulário e edição de texto-->
+            <div class="col bg-white p-4">
+                
+                <!-- Titúlo e subtitúlo do formulário: -->
+                <p class="text-start fs-5"><strong>Cadastro</strong></p>
+                <p class="text-start fs-6">Preencha os campos abaixo:</p>
+                <p>
+               
+                <form name="cadastro" method="GET" action="resposta.php">
+                    Nome: <input type="text" name="nome">
+                    <br><br>
+                    Endereço: <input type="text" name="endereco">
+                    <br><br>
+                    Bairro: <input type="text" name="bairro">
+                    <br><br>
+                    CEP: <input type="text" name="cep">
+                    <br><br>
+                    Estado: <input type="text" name="estado">
+                    <br><br>
+                    Cidade: <input type="text" name="cidade">
+                    <br><br>
+                    <input type="submit" name="cadastrar">
+                </form>
+         
+
+            </div>
+            <!-- Coluna criada para página responsiva -->
+            <div class="col-1">
+                &nbsp;
+            </div>
+        </div>
+    </div> 
 </body>
-</html>
